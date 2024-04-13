3bc5d0ab70e2407c7e61be4ef79bc1fdaf409bc8
Lorenzo <149068830+LorenzoDLaw@users.noreply.github.com>
2024-04-13 15:30:57 +0200

    upgrade funzione mostri e il loro movimento

--- src/it/edu/iisguiio/Mostro.java
+++ src/it/edu/iisguiio/Mostro.java
@@ -6,51 +6,298 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Mostro {
-    public Rectangle mostro;
-    public ImageView spriteMostro;
+
+    public Rectangle mostro1;
+    public Rectangle mostro2;
+    public Rectangle mostro3;
+    public ImageView spriteMostro1;
+    public ImageView spriteMostro2;
+    public ImageView spriteMostro3;
     private double maxWidth;
     private double maxHeight;
     private int tileSize; // Valore del tile
-    private int camminaMostro; // Passo corrente nel percorso del mostro
-    private double[] percorsoX; // Coordinate X del percorso
-    private double[] percorsoY; // Coordinate Y del percorso
     
-    
-    public Mostro(Rectangle mostro1, double maxWidth, double maxHeight, int tileSize, double[] percorsoX, double[] percorsoY,ImageView spriteMostro) {
-        this.mostro = mostro1;
+    // booleane per la direzione del mostro
+    boolean walkDown = false;
+    boolean walkLeft = false;
+    boolean walkRight = false;
+    boolean walkUp = false;
+
+    // coordinate del mostro per calcolare il suo spostamento
+    double xPrecedenteM1 = 0;
+    double yPrecedenteM1 = 0;
+    double xPrecedenteM2 = 0;
+    double yPrecedenteM2 = 0;
+    double xPrecedenteM3 = 0;
+    double yPrecedenteM3 = 0;
+
+    public Mostro(Rectangle rMostro1, Rectangle rMostro2, Rectangle rMostro3, double maxWidth, double maxHeight,
+            int tileSize, ImageView spriteMostro1, ImageView spriteMostro2,
+            ImageView spriteMostro3) {
+        this.mostro1 = rMostro1;
+        this.mostro2 = rMostro2;
+        this.mostro3 = rMostro3;
         this.maxWidth = maxWidth;
         this.maxHeight = maxHeight;
         this.tileSize = tileSize;
-        this.camminaMostro = 0;
-        this.percorsoX = percorsoX;
-        this.percorsoY = percorsoY;
-        this.spriteMostro = spriteMostro;
-        // Imposta il colore di riempimento trasparente
-        mostro.setFill(Color.TRANSPARENT);
-        // Imposta il colore del contorno trasparente
-        
+        this.spriteMostro1 = spriteMostro1;
+        this.spriteMostro2 = spriteMostro2;
+        this.spriteMostro3 = spriteMostro3;
     }
-    Image walk_up = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_up_1.png"));
+
+    // tutte le immagini che utilizzo per la camminata
+    Image walk_up_1 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_up_1.png"));
+    Image walk_up_2 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_up_2.png"));
+    Image walk_down_1 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_down_2.png"));
+    Image walk_down_2 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_down_2.png"));
+    Image walk_left_1 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_left_1.png"));
+    Image walk_left_2 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_left_2.png"));
+    Image walk_right_1 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_right_1.png"));
+    Image walk_right_2 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_right_2.png"));
 
     public void muoviMostro() {
-        // Se ci sono ancora passi nel percorso del mostro, muovi il mostro verso la prossima posizione nel percorso
-        spriteMostro.setImage(walk_up);
-        if (camminaMostro < percorsoX.length && camminaMostro < percorsoY.length) {
-            double newX = percorsoX[camminaMostro] * tileSize;
-            double newY = percorsoY[camminaMostro] * tileSize;
-            // Controlla che il mostro rimanga all'interno dei limiti del gioco
-            if (newX >= 0 && newX + mostro.getWidth() <= maxWidth && newY >= 0 && newY + mostro.getHeight() <= maxHeight) {
-                mostro.setX(newX);
-                mostro.setY(newY);
-                // Imposta la posizione dell'ImageView correttamente
-                spriteMostro.setX(newX);
-                spriteMostro.setY(newY);
-                camminaMostro++;
+        Mostro1();
+        Mostro2();
+        Mostro3();
+    }
+
+    public void Mostro1() {
+        // Dentro il ciclo di aggiornamento del gioco
+        int direction = (int) (Math.random() * 4) + 1; // Genera un numero casuale da 1 a 4
+
+        double newX = mostro1.getX();
+        double newY = mostro1.getY();
+
+        // Determina la nuova posizione in base alla direzione casuale
+        switch (direction) {
+        case 1: // Su
+            newY -= tileSize;
+            break;
+        case 2: // Giù
+            newY += tileSize;
+            break;
+        case 3: // Destra
+            newX += tileSize;
+            break;
+        case 4: // Sinistra
+            newX -= tileSize;
+            break;
+        }
+
+        // Controlla che il mostro rimanga all'interno dei limiti del gioco
+        if (newX >= 0 && newX + mostro1.getWidth() <= maxWidth && newY >= 0
+                && newY + mostro1.getHeight() <= maxHeight) {
+            mostro1.setX(newX);
+            mostro1.setY(newY);
+            // Imposta la posizione dell'ImageView correttamente
+            spriteMostro1.setX(newX);
+            spriteMostro1.setY(newY);
+        }
+
+        double deltaX = mostro1.getX() - xPrecedenteM1;
+        double deltaY = mostro1.getY() - yPrecedenteM1;
+        // viene utilizzato per capire la direzione del mostro in base alla sua posizione
+        // precedente
+        // poi inserisco lo sprite in base alla direzione
+        if (deltaY < 0) {
+            // Il mostro si muove verso l'alto
+            if (!walkUp) {
+                walkUp = true;
+                spriteMostro1.setImage(walk_up_2);
+            } else {
+                walkUp = false;
+                spriteMostro1.setImage(walk_up_1);
             }
-            if (camminaMostro == percorsoX.length) {
-                camminaMostro = 0;
+        } else if (deltaY > 0) {
+            // Il mostro si muove verso il basso
+            if (!walkDown) {
+                walkDown = true;
+                spriteMostro1.setImage(walk_down_2);
+            } else {
+                walkDown = false;
+                spriteMostro1.setImage(walk_down_1);
+            }
+        } else if (deltaX < 0) {
+            // Il mostro si muove verso sinistra
+            if (!walkLeft) {
+                walkLeft = true;
+                spriteMostro1.setImage(walk_left_2);
+            } else {
+                walkLeft = false;
+                spriteMostro1.setImage(walk_left_1);
+            }
+        } else if (deltaX > 0) {
+            // Il mostro si muove verso destra
+            if (!walkRight) {
+                walkRight = true;
+                spriteMostro1.setImage(walk_right_2);
+            } else {
+                walkRight = false;
+                spriteMostro1.setImage(walk_right_1);
             }
         }
+        // Aggiorna le coordinate precedenti
+        xPrecedenteM1 = mostro1.getX();
+        yPrecedenteM1 = mostro1.getY();
+    }
+
+    public void Mostro2() {
+        // Dentro il ciclo di aggiornamento del gioco
+        int direction = (int) (Math.random() * 4) + 1; // Genera un numero casuale da 1 a 4
+
+        double newX = mostro2.getX();
+        double newY = mostro2.getY();
+
+        // Determina la nuova posizione in base alla direzione casuale
+        switch (direction) {
+        case 1: // Su
+            newY -= tileSize;
+            break;
+        case 2: // Giù
+            newY += tileSize;
+            break;
+        case 3: // Destra
+            newX += tileSize;
+            break;
+        case 4: // Sinistra
+            newX -= tileSize;
+            break;
+        }
+
+        // Controlla che il mostro rimanga all'interno dei limiti del gioco
+        if (newX >= 0 && newX + mostro2.getWidth() <= maxWidth && newY >= 0
+                && newY + mostro2.getHeight() <= maxHeight) {
+            mostro2.setX(newX);
+            mostro2.setY(newY);
+            // Imposta la posizione dell'ImageView correttamente
+            spriteMostro2.setX(newX);
+            spriteMostro2.setY(newY);
+        }
+        double deltaX = mostro2.getX() - xPrecedenteM2;
+        double deltaY = mostro2.getY() - yPrecedenteM2;
+        // viene utilizzato per capire la direzione del mostro in base alla sua posizione
+        // precedente
+        // poi inserisco lo sprite in base alla direzione
+        if (deltaY < 0) {
+            // Il mostro si muove verso l'alto
+            if (!walkUp) {
+                walkUp = true;
+                spriteMostro2.setImage(walk_up_2);
+            } else {
+                walkUp = false;
+                spriteMostro2.setImage(walk_up_1);
+            }
+        } else if (deltaY > 0) {
+            // Il mostro si muove verso il basso
+            if (!walkDown) {
+                walkDown = true;
+                spriteMostro2.setImage(walk_down_2);
+            } else {
+                walkDown = false;
+                spriteMostro2.setImage(walk_down_1);
+            }
+        } else if (deltaX < 0) {
+            // Il mostro si muove verso sinistra
+            if (!walkLeft) {
+                walkLeft = true;
+                spriteMostro2.setImage(walk_left_2);
+            } else {
+                walkLeft = false;
+                spriteMostro2.setImage(walk_left_1);
+            }
+        } else if (deltaX > 0) {
+            // Il mostro si muove verso destra
+            if (!walkRight) {
+                walkRight = true;
+                spriteMostro2.setImage(walk_right_2);
+            } else {
+                walkRight = false;
+                spriteMostro2.setImage(walk_right_1);
+            }
+        }
+        // Aggiorna le coordinate precedenti
+        xPrecedenteM2 = mostro2.getX();
+        yPrecedenteM2 = mostro2.getY();
+    }
+
+    public void Mostro3() {
+        // Dentro il ciclo di aggiornamento del gioco
+        int direction = (int) (Math.random() * 4) + 1; // Genera un numero casuale da 1 a 4
+
+        double newX = mostro3.getX();
+        double newY = mostro3.getY();
+
+        // Determina la nuova posizione in base alla direzione casuale
+        switch (direction) {
+        case 1: // Su
+            newY -= tileSize;
+            break;
+        case 2: // Giù
+            newY += tileSize;
+            break;
+        case 3: // Destra
+            newX += tileSize;
+            break;
+        case 4: // Sinistra
+            newX -= tileSize;
+            break;
+        }
+
+        // Controlla che il mostro rimanga all'interno dei limiti del gioco
+        if (newX >= 0 && newX + mostro3.getWidth() <= maxWidth && newY >= 0
+                && newY + mostro3.getHeight() <= maxHeight) {
+            mostro3.setX(newX);
+            mostro3.setY(newY);
+            // Imposta la posizione dell'ImageView correttamente
+            spriteMostro3.setX(newX);
+            spriteMostro3.setY(newY);
+        }
+
+        double deltaX = mostro3.getX() - xPrecedenteM3;
+        double deltaY = mostro3.getY() - yPrecedenteM3;
+        // viene utilizzato per capire la direzione del mostro in base alla sua posizione
+        // precedente
+        // poi inserisco lo sprite in base alla direzione
+        if (deltaY < 0) {
+            // Il mostro si muove verso l'alto
+            if (!walkUp) {
+                walkUp = true;
+                spriteMostro3.setImage(walk_up_2);
+            } else {
+                walkUp = false;
+                spriteMostro3.setImage(walk_up_1);
+            }
+        } else if (deltaY > 0) {
+            // Il mostro si muove verso il basso
+            if (!walkDown) {
+                walkDown = true;
+                spriteMostro3.setImage(walk_down_2);
+            } else {
+                walkDown = false;
+                spriteMostro3.setImage(walk_down_1);
+            }
+        } else if (deltaX < 0) {
+            // Il mostro si muove verso sinistra
+            if (!walkLeft) {
+                walkLeft = true;
+                spriteMostro3.setImage(walk_left_2);
+            } else {
+                walkLeft = false;
+                spriteMostro3.setImage(walk_left_1);
+            }
+        } else if (deltaX > 0) {
+            // Il mostro si muove verso destra
+            if (!walkRight) {
+                walkRight = true;
+                spriteMostro3.setImage(walk_right_2);
+            } else {
+                walkRight = false;
+                spriteMostro3.setImage(walk_right_1);
+            }
+        }
+        // Aggiorna le coordinate precedenti
+        xPrecedenteM3 = mostro3.getX();
+        yPrecedenteM3 = mostro3.getY();
     }
 
 }
--- src/it/edu/iisguiio/Zelda.java
+++ src/it/edu/iisguiio/Zelda.java
@@ -2,22 +2,20 @@ package it.edu.iisguiio;
 
 import java.io.IOException;
 
+import javafx.animation.Animation;
+import javafx.animation.Animation.Status;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Bounds;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -42,21 +40,28 @@ public class Zelda extends Application {
     double Ypersonaggio=HEIGHT_GIOCO-TilesSize;
     //personaggio e nemici come rettangoli
     Rectangle personaggio = new Rectangle(Xpersonaggio,Ypersonaggio,TilesSize,TilesSize);
-    Rectangle mostro1= new Rectangle(TilesSize,TilesSize);
-    //percorso del mostro
-    double[] percorsoX = {2, 3, 3, 4, 5,5,5}; 
-    double[] percorsoY = {2, 2, 2, 2, 2,3,4};
+    Rectangle rMostro1= new Rectangle(TilesSize,TilesSize);
+    Rectangle rMostro2 = new Rectangle(TilesSize,TilesSize);
+    Rectangle rMostro3 = new Rectangle(TilesSize,TilesSize);
+    
     // ImageView per lo sprite del personaggio
-    ImageView spritePersonaggio;
     Image spriteImage = new Image(getClass().getResourceAsStream("Immagini/SpriteCamminata/walk_down_1.png"));
+    ImageView spritePersonaggio = new ImageView(spriteImage);
+    
     //sprite del mostro
-    ImageView spriteMostro;
     Image spriteImageMostro = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_down_2.png"));
+    ImageView spriteMostro= new ImageView(spriteImageMostro);
+    Image spriteImageMostro2 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_down_2.png"));
+    ImageView spriteMostro2= new ImageView(spriteImageMostro2);
+    Image spriteImageMostro3 = new Image(getClass().getResourceAsStream("Immagini/Monster/skeletonlord_down_2.png"));
+    ImageView spriteMostro3= new ImageView(spriteImageMostro3);
+    
     // classe 
     Personaggio richiamaPersonaggio; 
     Mostro richiamaMostro;
-    int VitaPersonaggio;
     
+    int VitaPersonaggio=3;
+    int countMostri=1;
     // creo i pane per il gioco e il munù
     Pane paneMenù= new Pane();
     Pane paneWord = new Pane();
@@ -67,45 +72,60 @@ public class Zelda extends Application {
 
     Stage finestra;
     public void start(Stage primaryStage) throws Exception {
-    	finestra=primaryStage;
-    	VitaPersonaggio=3;
+    	finestra=primaryStage;;
         // Creo la griglia
         paneMenù.setPrefSize(WIDTH_GIOCO, HEIGHT_GIOCO);
     	paneWord.setPrefSize(WIDTH_GIOCO, HEIGHT_GIOCO);
         Scene scene = new Scene(paneWord);
-
+        
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        //sfondo 
+        // Carica l'immagine di sfondo
+        Image backgroundImage = new Image(getClass().getResourceAsStream("Immagini/mappa.jpg"));
+
+        // Crea un ImageView per l'immagine di sfondo
+        ImageView backgroundImageView = new ImageView(backgroundImage);
+
+        // Imposta la dimensione dell'ImageView come la dimensione della scena
+        backgroundImageView.setFitWidth(WIDTH_GIOCO);
+        backgroundImageView.setFitHeight(HEIGHT_GIOCO);
+
+        // Aggiungi l'ImageView di sfondo al paneWord in modo che sia dietro agli altri elementi
+        paneWord.getChildren().add(0, backgroundImageView);
+
         timelineGioco.setCycleCount(Timeline.INDEFINITE);
 
-        // Carica l'immagine dello sprite del personaggio
-        spritePersonaggio = new ImageView(spriteImage);
-        spriteMostro = new ImageView(spriteImageMostro);
         // Imposta la posizione iniziale dello sprite
         spritePersonaggio.setX(Xpersonaggio);
         spritePersonaggio.setY(Ypersonaggio);
-        // Imposta la dimensione dello sprite 
+        
+        // Imposta la dimensione degli sprite 
         spritePersonaggio.setFitWidth(TilesSize);
         spritePersonaggio.setFitHeight(TilesSize);
         spriteMostro.setFitWidth(TilesSize);
         spriteMostro.setFitHeight(TilesSize);
+        spriteMostro2.setFitWidth(TilesSize);
+        spriteMostro2.setFitHeight(TilesSize);
+        spriteMostro3.setFitWidth(TilesSize);
+        spriteMostro3.setFitHeight(TilesSize);
         // Aggiungi lo sprite al Pane del gioco
         paneWord.getChildren().addAll(personaggio, spritePersonaggio);
         
         // Inizializza il gestore del personaggio
         richiamaPersonaggio = new Personaggio(personaggio, WIDTH_GIOCO, HEIGHT_GIOCO, spritePersonaggio, TilesSize);
-        richiamaMostro = new Mostro(mostro1, WIDTH_GIOCO, HEIGHT_GIOCO, TilesSize,percorsoX, percorsoY,spriteMostro);
-        //posizione il mostro
-        mostro1.setFill(Color.RED); // Colore rosso per il mostro (puoi cambiarlo a tuo piacimento)
-        mostro1.setStroke(Color.TRANSPARENT);
-        mostro1.setX(WIDTH_GIOCO / 4); // Posizione iniziale del mostro
-        mostro1.setY(HEIGHT_GIOCO / 4);
-        spriteMostro.setX(HEIGHT_GIOCO/4);
+        richiamaMostro = new Mostro(rMostro1, rMostro2, rMostro3, WIDTH_GIOCO, HEIGHT_GIOCO, TilesSize, spriteMostro, spriteMostro2, spriteMostro3);
+        //posizione il mostro e do un colore al rettangolo
+        rMostro1.setFill(Color.TRANSPARENT); // lo rendo trasparente cosi non sara visibile ma sara usato per le collisioni
+        rMostro1.setStroke(Color.TRANSPARENT);
+        rMostro1.setX(WIDTH_GIOCO / 4); // Posizione iniziale del mostro
+        rMostro1.setY(HEIGHT_GIOCO / 4);
+        spriteMostro.setX(HEIGHT_GIOCO / 4);
         spriteMostro.setY(HEIGHT_GIOCO / 4);
         // Aggiungi il mostro al gioco
-        paneWord.getChildren().addAll(mostro1, spriteMostro);
-
+        paneWord.getChildren().addAll(rMostro1, spriteMostro);
+        
         scene.setOnKeyPressed(this::premiTasto);
         
         //permette di richiamare la funzione all'inizio
@@ -113,11 +133,13 @@ public class Zelda extends Application {
     }
     
     public void premiTasto(KeyEvent e) {
-        richiamaPersonaggio.muovi(e);
-        if(e.getCode()==KeyCode.ESCAPE) {
-        	timelineGioco.stop();
-        	AvviaMenù();
-        }
+        //if(timelineGioco.getStatus()==Status.RUNNING) {
+        	richiamaPersonaggio.muovi(e);
+            if(e.getCode()==KeyCode.ESCAPE) {
+            	timelineGioco.stop();
+            	AvviaMenù();
+            }
+        //}    	
     } 
     Button bStart = new Button("inizia la tua avventura");
     Button bComandi= new Button("comandi");
@@ -138,7 +160,6 @@ public class Zelda extends Application {
     	bComandi.setOnAction(e-> mostraComandi());
     }
     public void iniziaAvventura() {
-    	
     	timelineGioco.play();
     	paneComandi.getChildren().clear();
     	paneMenù.getChildren().clear();
@@ -157,20 +178,100 @@ public class Zelda extends Application {
     public void aggiornaGioco(){
         // Muovi il mostro
         richiamaMostro.muoviMostro();
-        Shape intersect = Shape.intersect(personaggio, mostro1);
-        // Verifica la collisione tra il personaggio e il mostro
-        if(intersect.getBoundsInLocal().getWidth() != -1) {
+        
+        // Verifica la collisione con i mostri
+        Shape intersect1 = Shape.intersect(personaggio, rMostro1);
+        Shape intersect2 = Shape.intersect(personaggio, rMostro2);
+        Shape intersect3 = Shape.intersect(personaggio, rMostro2);
+        boolean mostro1Eliminato=false;
+        boolean mostro2Eliminato=false;
+        boolean mostro3Eliminato=false;
+        // Verifica la collisione tra il personaggio e il primo mostro
+        if(intersect1.getBoundsInLocal().getWidth() != -1) {
+            if (richiamaPersonaggio.HoAttaccato==true) {
+                paneWord.getChildren().removeAll(spriteMostro,rMostro1);
+                mostro1Eliminato=true;
+                countMostri++;
+            } else {
+                if (VitaPersonaggio == 0) {
+                    paneWord.getChildren().removeAll(personaggio, spritePersonaggio);
+                } else {
+                    VitaPersonaggio--;
+                }   
+            }      
+        }
+        if(intersect2.getBoundsInLocal().getWidth() != -1) {
         	if (richiamaPersonaggio.HoAttaccato==true) {
-        		timelineGioco.stop();
-        		paneWord.getChildren().remove(mostro1);
-        	}else {
-        		if (VitaPersonaggio==0) {
-        			paneWord.getChildren().removeAll(personaggio, spritePersonaggio);
-        		}else {
-        			VitaPersonaggio--;
-        		}	
-        	}
-        		
+                paneWord.getChildren().removeAll(spriteMostro2,rMostro2);
+                countMostri++;
+                mostro2Eliminato=true;
+            } else {
+                if (VitaPersonaggio == 0) {
+                    paneWord.getChildren().removeAll(personaggio, spritePersonaggio);
+                } else {
+                    VitaPersonaggio--;
+                }   
+            }
+        }
+        if(intersect3.getBoundsInLocal().getWidth() != -1) {
+        	if (richiamaPersonaggio.HoAttaccato==true) {
+                paneWord.getChildren().removeAll(spriteMostro3,rMostro3);
+                mostro3Eliminato=true;
+            } else {
+                if (VitaPersonaggio == 0) {
+                    paneWord.getChildren().removeAll(personaggio, spritePersonaggio);
+                } else {
+                    VitaPersonaggio--;
+                }   
+            }
+        }
+        if(countMostri==2 && mostro1Eliminato==true) {
+        	mostro1Eliminato=false;
+        	aggiungiMostro();
+        }
+        if(countMostri==3 && mostro1Eliminato==true && mostro2Eliminato==true) {
+        	mostro1Eliminato=false; 
+        	mostro2Eliminato=false;
+        	aggiungiMostro();
+        } 
+        if(countMostri>=4 && mostro1Eliminato==true && mostro2Eliminato==true && mostro3Eliminato==true) {
+        	mostro1Eliminato=false; 
+        	mostro2Eliminato=false;
+        	mostro3Eliminato=false; 
+        	aggiungiMostro();
+        } 
+    }
+
+    // Funzione per aggiungere un nuovo mostro se necessario
+    private void aggiungiMostro() {
+        if (countMostri ==2) {
+        	int posX = (int) Math.random()*gameColoum;
+        	int posY = (int) Math.random()*gameRow;
+        	//posizione il mostro e do un colore al rettangolo
+            rMostro2.setFill(Color.RED); // lo rendo trasparente cosi non sara visibile ma sara usato per le collisioni
+            rMostro2.setStroke(Color.RED);
+            rMostro2.setX(posX*TilesSize); // Posizione iniziale del mostro
+            rMostro2.setY(posY*TilesSize);
+            spriteMostro2.setX(posX*TilesSize);
+            spriteMostro2.setY(posY*TilesSize);
+        	paneWord.getChildren().addAll(spriteMostro, rMostro1, spriteMostro2, rMostro2);
+            countMostri++;
+            richiamaMostro.muoviMostro();
+            //timelineGioco.play();
+        }
+        if(countMostri==3) {
+        	int posX = (int) Math.random()*gameColoum;
+        	int posY = (int) Math.random()*gameRow;
+        	//posizione il mostro e do un colore al rettangolo
+            rMostro3.setFill(Color.RED); // lo rendo trasparente cosi non sara visibile ma sara usato per le collisioni
+            rMostro3.setStroke(Color.RED);
+            rMostro3.setX(posX*TilesSize); // Posizione iniziale del mostro
+            rMostro3.setY(posY*TilesSize);
+            spriteMostro3.setX(posX*TilesSize);
+            spriteMostro3.setY(posY*TilesSize);
+        	paneWord.getChildren().addAll(spriteMostro,rMostro1,spriteMostro2, rMostro2,spriteMostro3, rMostro3);
+        	richiamaMostro.muoviMostro();
+        	//timelineGioco.play();
         }
     }
     
